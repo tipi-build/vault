cc4e50d4151edbf58e4e1ef062175d3886093fee
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2021-07-22 12:51:43 +0200

    :wink: integration of the native vault in the onboarding session (still some missing implementation)

--- tipi/vault.hpp
+++ tipi/vault.hpp
@@ -1,14 +1,14 @@
 #pragma once 
 
+#include <iostream>
+#include <map>
 #include <gh/auth.hxx>
 #include <pre/json/from_json.hpp>
-#include <map>
+#include <pre/json/to_json.hpp>
 
 #include <tipi/detail/vault_impl.hpp>
 
 namespace tipi {
-
-	using registered_users_t = std::map<std::string, bool>;
 
   struct auth_t {
     gh::auth auth_info;
@@ -30,15 +30,36 @@ namespace tipi {
 	 * 
 	 */
 	struct vault {
-		vault(const std::string& encrypted_buffer, const std::string& password) :
-			encrypted_buffer_(encrypted_buffer),
-			password_(password)
+
+		//! Creates a default empty vault with the given vault key.
+		vault(const std::string& vault_key) :
+			vault_key_(vault_key),
+			encrypted_buffer_("[]")
 		{}
 
+		//! Loads a vault with the given key from the provided encrypted_buffer.
+		vault(const std::string& vault_key, const std::string& encrypted_buffer) :
+			vault_key_(vault_key),
+			encrypted_buffer_(encrypted_buffer)
+		{}
+
+		void add_auth(const std::string& user, const std::string& password/*, 
+			const std::optional<std::string>& endpoint = std::nullopt*/ ) {
+			//TODO: decrypt_vault(vault_key)
+			std::cout << "Adding : " << user << " , " << password << std::endl;
+			auto auths = pre::json::from_json<auths_t>(encrypted_buffer_);
+			std::cout << "auths deserialized : " << auths.size() << std::endl;
+			auths.push_back(auth_t{ gh::auth{user, password}, std::nullopt });
+			std::cout << "auths size : " << auths.size() << std::endl;
+			std::cout << pre::json::to_json(auths) << std::endl;
+			//TODO: encrypt_vault(vault_key)
+			encrypted_buffer_ = pre::json::to_json(auths);
+		}
+
 		auths_t auths() const {
-			//TODO: decrypt_vault(password)
-			//TODO: deserizalize(vault)
-			return { {.auth_info= gh::auth{"booom", "bar"}} };
+			//TODO: decrypt_vault(vault_key)
+			auto auths = pre::json::from_json<auths_t>(encrypted_buffer_);
+			return auths;
 		}
 
 		std::string get_encrypted_buffer() const {
@@ -49,20 +70,20 @@ namespace tipi {
 			encrypted_buffer_ = encrypted_buffer;
 		}
 
-		//! Changes the password and reencrypts the vault accordingly.
-		void password(const std::string& new_password) { 
-			auto plain_vault = detail::decrypt_vault(password_, encrypted_buffer_);
+		////! Changes the vault_key and reencrypts the vault accordingly.
+		//void vault_key(const std::string& new_vault_key) { 
+		//	auto plain_vault = detail::decrypt_vault(vault_key_, encrypted_buffer_);
 
-			password_ = new_password;
+		//	vault_key_ = new_vault_key;
 
-			encrypted_buffer_ = detail::encrypt_vault(password_, plain_vault);
-		}
+		//	encrypted_buffer_ = detail::encrypt_vault(vault_key_, plain_vault);
+		//}
 
-		std::string password() const { return password_; }
+		//std::string vault_key() const { return vault_key_; }
 
 		private: 
+		std::string vault_key_;
 		std::string encrypted_buffer_;
-		std::string password_;
 	};
 
 
@@ -75,9 +96,11 @@ namespace tipi {
 	using namespace emscripten;
 	EMSCRIPTEN_BINDINGS(tipi_vault) {
 	  class_<tipi::vault>("tipi_vault")
+	    .constructor<std::string>()
 	    .constructor<std::string, std::string>()
 	    .function("auths", &tipi::vault::auths)
+	    .function("add_auth", &tipi::vault::add_auth)
 	    .property("encrypted_buffer", &tipi::vault::get_encrypted_buffer, &tipi::vault::set_encrypted_buffer)
-	    //.property("password", &tipi::vault::password, &tipi::vault::password)
+	    //.property("vault_key", &tipi::vault::vault_key, &tipi::vault::vault_key)
 	    ;
 }
