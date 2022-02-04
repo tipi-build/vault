b49872d54b9d3a2c0ab0b7dbd65a267785cee024
Yannic Staudt <yannic@staudt-engineering.com>
2022-02-04 21:42:56 +0100

    :unicorn: A crazy first take of a vault v2 that can patch itself and has a public/private key pair built in :P

--- tipi/detail/encryption.hpp
+++ tipi/detail/encryption.hpp
@@ -7,6 +7,18 @@
 #include <cryptopp/filters.h>
 #include <cryptopp/files.h>
 #include <cryptopp/base64.h>
+#include <cryptopp/chachapoly.h>
+#include <cryptopp/hkdf.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/eccrypto.h>
+#include <cryptopp/osrng.h>
+#include <cryptopp/xed25519.h>
+#include <cryptopp/donna.h>
+#include <cryptopp/hex.h>
+
+#include <xxhr/util.hpp> // for base64
+
+#include "../vault.hpp"
 
 namespace tipi::detail {
 
@@ -17,6 +29,8 @@ using CryptoPP::StringSink;
 using CryptoPP::FileSource;
 using CryptoPP::Base64Decoder;
 using CryptoPP::Base64Encoder;
+using CryptoPP::HexEncoder;
+using CryptoPP::HexDecoder;
 
 using CryptoPP::AES;
 using CryptoPP::SHA256;
@@ -25,6 +39,13 @@ using CryptoPP::DataParametersInfo;
 using CryptoPP::DataEncryptorWithMAC;
 using CryptoPP::DataDecryptorWithMAC;
 using CryptoPP::SimpleProxyFilter;
+using CryptoPP::XChaCha20Poly1305;
+using CryptoPP::HKDF;
+using CryptoPP::ECIES;
+using CryptoPP::ECP;
+using CryptoPP::Name::Pad;
+using CryptoPP::Name::InsertLineBreaks;
+
 
 const size_t SALT_SIZE = 8;
 const size_t ITERATIONS = 2500;
@@ -35,40 +56,242 @@ using DataEncryptorTipiVault = DataEncryptorWithMAC <
 		AES::BLOCKSIZE, AES::DEFAULT_KEYLENGTH, HMAC<SHA256>::DIGESTSIZE, SALT_SIZE, ITERATIONS 
 	>>;
 
-using DataDecryptorTipiVault = DataDecryptorWithMAC <
+using Vault_v1_DataDecryptorWithMAC = DataDecryptorWithMAC <
 	AES,SHA256,
 	HMAC<SHA256>,
 	DataParametersInfo<
 		AES::BLOCKSIZE, AES::DEFAULT_KEYLENGTH, HMAC<SHA256>::DIGESTSIZE, SALT_SIZE, ITERATIONS 
 	>>;
 
+	struct AEAD_decryption_result {
+		std::string plain_text;
+		std::string authenticated_data;
+		bool is_legacy_vault;
+	};
 
-	inline std::string encrypt(const std::string& password, const std::string& plain_buffer) {
-		std::string encrypted;
-	 	StringSource ss(plain_buffer, true,
-			new DataEncryptorTipiVault(
-					reinterpret_cast<const uint8_t*>(password.data()), password.size(),
-          new Base64Encoder(
-            new StringSink(encrypted),
-            false /* no newline */
-          )
-				)
-		); 
+	struct ecies_keypair {
+		std::string private_key;
+		std::string public_key;
+	};
+
+	const std::string v2vault_preamble = "v2:";
+	const std::string v2vault_separator = ":";
+
+	inline std::string encrypt(const std::string& password_str, const std::string& plain_buffer, const std::string& additional_authenticated_data) {
+		// init the PRNG
+		std::random_device r;
+		std::seed_seq seed{r(), r(), r(), r(), r(), r(), r(), r()}; 
+		std::mt19937 prng(seed);
+
+		// encrypt the data using cryptopp's xchachapoly implementation
+		XChaCha20Poly1305::Encryption encryption_engine;
+
+		// create a nonce
+		std::string nonce_str(encryption_engine.IVSize(), char{});
+		std::generate(nonce_str.begin(), nonce_str.end() - 1, prng);
+		const uint8_t* nonce = reinterpret_cast<const uint8_t*>(nonce_str.c_str());
+
+		// derive the key from the password using HKDF 
+		const uint8_t* password = reinterpret_cast<const uint8_t*>(password_str.c_str());
+
+		uint8_t* key{ new uint8_t[encryption_engine.DefaultKeyLength()]{} };
+		HKDF<SHA256> hkdf;
+		hkdf.DeriveKey(key, encryption_engine.DefaultKeyLength(), 
+			password, password_str.length(), 
+			nonce, nonce_str.length(),
+			NULL, 0
+		);
+
+		// encrypt everything
+		const uint8_t* plain_text = reinterpret_cast<const uint8_t*>(plain_buffer.c_str());
+		const uint8_t* aad = reinterpret_cast<const uint8_t*>(additional_authenticated_data.c_str());		
+		uint8_t* cyphertext{ new uint8_t[plain_buffer.length()]{} };
+		uint8_t* mac{ new uint8_t[encryption_engine.DigestSize()]{} };
+
+		encryption_engine.SetKeyWithIV(key, encryption_engine.DefaultKeyLength(), nonce, encryption_engine.IVSize());
+		encryption_engine.EncryptAndAuthenticate(cyphertext, 
+			mac, encryption_engine.DigestSize(), 
+			nonce, encryption_engine.IVSize(), 
+			aad, additional_authenticated_data.length(), 
+			plain_text, plain_buffer.length());
+		
+
+		std::string ct_str = std::string(reinterpret_cast<char const*>(cyphertext), plain_buffer.length());
+		std::string mac_str = std::string(reinterpret_cast<char const*>(mac), encryption_engine.DigestSize());
+
+		// v2 vault is formated like:
+		// v2:<nonce>:<BASE64 encoded XChaCha20-Poly1305 encrypted message>:<MAC>:<base64 encoded AAD (additional authenticated data)>
+		std::string encrypted = v2vault_preamble + xxhr::util::encode64(nonce_str) 
+			+ v2vault_separator + xxhr::util::encode64(ct_str)
+			+ v2vault_separator + xxhr::util::encode64(mac_str)
+			+ v2vault_separator + xxhr::util::encode64(additional_authenticated_data);
 
 		return encrypted;
 	} 
 
-	inline std::string decrypt(const std::string& password, const std::string& encrypted_buffer) {
-		std::string plain_buffer;
-		StringSource ss(encrypted_buffer, true,
-        new Base64Decoder(
-					new DataDecryptorTipiVault(
-						reinterpret_cast<const uint8_t*>(password.data()), password.size(),
-						new StringSink(plain_buffer)
-					)
-        )
-		);		
+	inline AEAD_decryption_result decrypt(const std::string& password_str, const std::string& input) {
+		AEAD_decryption_result result;
 
-		return plain_buffer;
+		const size_t v2vault_separator_len = v2vault_separator.length();		
+		const size_t v2vault_preamble_len = v2vault_preamble.length();
+
+		if(input.rfind(v2vault_preamble, 0) == 0) {
+			const uint8_t* password = reinterpret_cast<const uint8_t*>(password_str.c_str());
+			
+			//
+			// v2 vault is formated like:
+			// v2:<nonce>:<BASE64 encoded XChaCha20-Poly1305 encrypted message>:<MAC>:<base64 encoded AAD (additional authenticated data)>
+			//
+
+			/* Getting the nonce / IV */
+			size_t nonce_end   = input.find(v2vault_separator, v2vault_preamble_len);	// find the second separator
+			std::string nonce_str = xxhr::util::decode64(input.substr(v2vault_preamble_len, nonce_end  - v2vault_preamble_len));
+			const uint8_t* nonce = reinterpret_cast<const uint8_t*>(nonce_str.c_str());
+
+			/* the encrypted part */
+			size_t cyphertext_start_offset = nonce_end + v2vault_separator_len;
+			size_t cyphertext_end   = input.find(v2vault_separator, cyphertext_start_offset);	// find the 3rd separator
+			std::string cyphertext_str = xxhr::util::decode64(input.substr(cyphertext_start_offset, cyphertext_end - cyphertext_start_offset));
+			const uint8_t* cyphertext = reinterpret_cast<const uint8_t*>(cyphertext_str.c_str());
+
+			/* the MAC part */
+			size_t mac_start_offset = cyphertext_end + v2vault_separator_len;
+			size_t mac_end    = input.find(v2vault_separator, mac_start_offset);	// find the 4th separator
+			std::string mac_str = xxhr::util::decode64(input.substr(mac_start_offset, mac_end - mac_start_offset));
+			const uint8_t*  mac = reinterpret_cast<const uint8_t*>(mac_str.c_str());
+
+			/* the AAD part */
+			size_t aad_start_offset = mac_end + v2vault_separator_len;
+			std::string aad_str = xxhr::util::decode64(input.substr(aad_start_offset));
+			const uint8_t*  aad = reinterpret_cast<const uint8_t*>(aad_str.c_str());
+
+			// decrypt the data using cryptopp's xchachapoly implementation
+			XChaCha20Poly1305::Decryption decryption_engine;
+
+			// derive the key using HKDF 
+			// using the same nonce a used above
+			uint8_t* key{ new uint8_t[decryption_engine.DefaultKeyLength()]{} };
+			HKDF<SHA256> hkdf;
+			hkdf.DeriveKey(key, decryption_engine.DefaultKeyLength(), 
+				password, password_str.length(), 
+				nonce, nonce_str.length(),
+				NULL, 0
+			);
+
+			// initialize & decrypt the actual message
+			uint8_t* recovered_text{ new uint8_t[cyphertext_str.length()]{} };
+
+			decryption_engine.SetKeyWithIV(key, decryption_engine.DefaultKeyLength(), nonce, decryption_engine.IVSize());
+			bool decryption_succeeeded = decryption_engine.DecryptAndVerify(
+				recovered_text, 
+				mac, mac_str.length(), 
+				nonce, nonce_str.length(), 
+				aad, aad_str.length(), 
+				cyphertext, cyphertext_str.length()
+			);
+    		
+			if(decryption_succeeeded) {				
+				result.plain_text = std::string(reinterpret_cast<char const*>(recovered_text), cyphertext_str.length());
+			}
+			else {
+				throw std::runtime_error("Failed to decrypt vault");
+			}
+
+			result.authenticated_data = aad_str;
+			result.is_legacy_vault = false;
+		}
+		else {
+			// handle the "legacy" vault
+			std::string plain_buffer;
+			StringSource ss(input, true,
+			new Base64Decoder(
+						new Vault_v1_DataDecryptorWithMAC(
+							reinterpret_cast<const uint8_t*>(password_str.data()), password_str.size(),
+							new StringSink(plain_buffer)
+						)
+			)
+			);
+
+			result.plain_text = plain_buffer;
+			result.authenticated_data = "";
+			result.is_legacy_vault = true;
+		}
+		return result;
+	}
+
+	//! \brief retrieve tail / AAD part of the encrypted buffer
+	inline std::string get_aad_part(const std::string &input) {
+		
+		// look for the "last" ":" / v2vault_separator 
+		int mac_end = input.rfind(v2vault_separator);
+
+		// base64 decode that stuff...
+		return xxhr::util::decode64(input.substr(mac_end + v2vault_separator.length()));
+	} 
+
+	inline ecies_keypair generate_ecies_keypair() {
+		CryptoPP::AutoSeededRandomPool prng;
+		ECIES<ECP>::Decryptor decryptor(prng, CryptoPP::ASN1::secp256r1()); // curve25519()
+		ECIES<ECP>::Encryptor encryptor(decryptor);
+		
+		std::string private_key, public_key;
+		CryptoPP::HexEncoder hexenc_private(new CryptoPP::StringSink(private_key));
+		CryptoPP::HexEncoder hexenc_public(new CryptoPP::StringSink(public_key));
+
+		encryptor.GetPublicKey().Save(hexenc_public);
+		decryptor.GetPrivateKey().Save(hexenc_private);
+
+		ecies_keypair result;
+		result.private_key = private_key;
+		result.public_key = public_key;
+
+		return result;
+	}
+
+	inline std::string decrypt_ecies_message(const std::string &cyphertext_hex, const std::string &private_key_hex) {
+		CryptoPP::AutoSeededRandomPool prng;
+
+		std::cout << "cyphertext: " << cyphertext_hex << std::endl;
+
+		ECIES<ECP>::Decryptor decryptor(prng, CryptoPP::ASN1::secp256r1());
+
+		CryptoPP::StringSource priv_key_source(private_key_hex, true, new CryptoPP::HexDecoder());		
+		decryptor.AccessPrivateKey().Load(priv_key_source);
+		decryptor.AccessPrivateKey().ThrowIfInvalid(prng, 3);
+		std::cout << "loaded private key" << std::endl;
+
+		std::string cyphertext_plain;		
+		CryptoPP::StringSource ss(cyphertext_hex, true,
+			new CryptoPP::HexDecoder(
+				new CryptoPP::StringSink(cyphertext_plain)
+			) // HexDecoder
+		); // StringSource
+
+		std::cout << "cyphertext plain: " << cyphertext_plain << std::endl;
+
+		std::string cleartext; // decrypted message
+    	CryptoPP::StringSource s (cyphertext_plain, true, new CryptoPP::PK_DecryptorFilter(prng, decryptor, new CryptoPP::StringSink(cleartext) ) );
+
+		std::cout  << "cyphertext plain: " << cleartext << std::endl;
+		return cleartext;
+	}
+
+
+	inline std::string encrypt_ecies_message(const std::string &cleartext, const std::string &public_key_hex) {
+		CryptoPP::AutoSeededRandomPool prng;		
+
+		// load the key...
+		CryptoPP::StringSource pub_key_source(public_key_hex, true, new CryptoPP::HexDecoder());
+
+		// init the encryptor
+		ECIES<ECP>::Encryptor encryptor;
+		encryptor.AccessPublicKey().Load(pub_key_source);
+		encryptor.AccessPublicKey().ThrowIfInvalid(prng, 3);
+
+		std::string encrypted_buffer; // encrypted message
+    	CryptoPP::StringSource s (cleartext, true, new CryptoPP::PK_EncryptorFilter(prng, encryptor, new CryptoPP::HexEncoder(new CryptoPP::StringSink(encrypted_buffer) ) ) );
+		std::cout << "ency:" << encrypted_buffer << std::endl;
+
+		return encrypted_buffer;
 	}
 }
--- tipi/vault.cpp
+++ tipi/vault.cpp
@@ -23,6 +23,7 @@
       enum_<endpoint_t>("endpoint_t")
           .value("GITHUB", endpoint_t::GITHUB)
           .value("TIPI_STORAGE", endpoint_t::TIPI_STORAGE)
+          .value("TIPI_VAULT_PPK", endpoint_t::TIPI_VAULT_PPK)
       ;
 
       value_object<tipi::auth_t>("auth_t")
@@ -33,10 +34,27 @@
         ;
       register_vector<tipi::auth_t>("vector_auth_t");
 
+      value_object<tipi::auth_patch_t>("auth_patch_t")
+        .field("patch_hash", &tipi::auth_patch_t::patch_hash)
+        .field("parent_hash", &tipi::auth_patch_t::parent_hash)
+        .field("auth_entry", &tipi::auth_patch_t::encrypted_patch)
+        ;
+
+      value_object<tipi::vault_info_t>("vault_info_t")
+        .field("public_key", &tipi::vault_info_t::public_key)
+        .field("revision", &tipi::vault_info_t::revision)
+        ;
+
+      value_object<tipi::access_key_info_t>("access_key_info_t")
+        .field("revision", &tipi::access_key_info_t::revision)
+        ;
+
       class_<vault_access_key>("vault_access_key")
         .constructor<std::string>()
         .constructor<std::string, std::string>()
         .function("regenerate", &vault_access_key::regenerate)
+        .function("update", &vault_access_key::update)
+        .function("get_info", &vault_access_key::get_info)
         .property("encrypted_buffer", &vault_access_key::get_encrypted_buffer, &vault_access_key::set_encrypted_buffer)
         .property("passphrase", &vault_access_key::get_passphrase, &vault_access_key::set_passphrase)
         .property("raw_key", &vault_access_key::get_raw_key,  &vault_access_key::set_raw_key)
@@ -47,9 +65,13 @@
         .constructor<const vault_access_key&, std::string>()
         .function("add", &vault::add)
         .function("remove", &vault::remove)
+        .function("apply_patch", &vault::apply_patch)
+        .function("create_patch", &vault::create_patch)
+        .function("get_info", &vault::get_info)
+        .function("update", &vault::update)
         .property("auths", &vault::get_auths, &vault::set_auths)
-        .property("encrypted_buffer", &vault::get_encrypted_buffer, &vault::set_encrypted_buffer)
-        ;
+        .property("encrypted_buffer", &vault::get_encrypted_buffer, &vault::set_encrypted_buffer)        
+        ;      
     }
     }
   }
@@ -61,7 +83,10 @@ namespace tipi {
     access_key_(access_key)
   {
     auths_t auths{};
-    set_auths(auths);
+    vault_info_t vault_info{};
+
+    save_vault_internal(auths, vault_info);
+    update();
   }
 
   //! Loads a vault with the given key from the provided encrypted_buffer.
@@ -71,26 +96,174 @@ namespace tipi {
   {}
 
   void vault::add(const auth_t& auth) {
-    auto auths = get_auths();
-    auths.push_back(auth);
-    set_auths(auths);
+    deserialized_vault_data_t dv = decrypt_and_deserialize_vault();
+    dv.auths.push_back(auth);
+    save_vault_internal(dv.auths, dv.vault_info);
   }
 
   void vault::remove(const auth_t& auth) {
-    auto auths = get_auths();
+    deserialized_vault_data_t dv = decrypt_and_deserialize_vault();
 
-    auto found = std::find(auths.begin(), auths.end(), auth);
-    if (found != auths.end()) { auths.erase(found); }
-    set_auths(auths);
+    auto found = std::find(dv.auths.begin(), dv.auths.end(), auth);
+    if (found != dv.auths.end()) { dv.auths.erase(found); }
+    
+    save_vault_internal(dv.auths, dv.vault_info);
   }
 
   auths_t vault::get_auths() const {
-    return pre::json::from_json<auths_t>(detail::decrypt(access_key_.get_raw_key(), encrypted_buffer_));
+    deserialized_vault_data_t dv = decrypt_and_deserialize_vault();
+    return dv.auths;
   }
 
   void vault::set_auths(const auths_t& auths) {
-    encrypted_buffer_ = detail::encrypt(access_key_.get_raw_key(), pre::json::to_json(auths).dump() );
+    deserialized_vault_data_t dv = decrypt_and_deserialize_vault();
+    save_vault_internal(auths, dv.vault_info);
+  }
 
+  bool vault::apply_patch(const auth_patch_t& patch) {
+    auto decr_result = detail::decrypt(access_key_.get_raw_key(), encrypted_buffer_);
+
+    auths_t auths = pre::json::from_json<auths_t>(decr_result.plain_text);
+    vault_info_t vault_info;
+    auth_t ppk_entry;
+
+    // get the first (*sic*) ppk vault entry
+      auto vault_ppk_enty = std::find_if(auths.begin(), auths.end(), 
+        [](auth_t a){ return a.type == tipi::endpoint_t::TIPI_VAULT_PPK; }); 
+
+    // OH NO! we don't have a public/private key set
+    if(vault_ppk_enty == auths.end()) {     
+      ppk_entry = generate_ecies_keypair_auth();
+      auths.push_back(ppk_entry);   
+    }
+    else {
+      ppk_entry = *vault_ppk_enty;
+    }
+
+    if(decr_result.is_legacy_vault) {
+      vault_info.public_key = ppk_entry.user; // "user" is the public key...
+      vault_info.revision = "";
+    }
+    else {
+      vault_info = pre::json::from_json<vault_info_t>(decr_result.authenticated_data);
+    }
+
+    if(vault_info.revision == patch.patch_hash) {
+      return true;  // already applied - done.
+    }
+
+    // decrypt and deserialize the patch
+    std::string patch_ct = detail::decrypt_ecies_message(patch.encrypted_patch, ppk_entry.pass);
+    auth_t patch_auth = pre::json::from_json<auth_t>(patch_ct);
+
+    // check if it can be applied & apply if so
+    if(patch.parent_hash == vault_info.revision) {
+      
+      // remove existing on with the same endpoint
+      auto existing_auth_itt = std::find_if(auths.begin(), auths.end(), 
+        [&](auth_t a){ return a.endpoint == patch_auth.endpoint; }); 
+
+      if(existing_auth_itt != auths.end()) {
+        auths.erase(existing_auth_itt);
+      }
+
+      // finally add the new one, update vault info and save
+      auths.push_back(patch_auth);
+      vault_info.revision = patch.patch_hash;
+
+      save_vault_internal(auths, vault_info, false);  // don't re-calc the hash to not loose subsequent updates in a sequence
+      return true;
+    }
+
+    return false;
+  }
+  
+  void vault::save_vault_internal(const auths_t& auths, vault_info_t vault_info, const bool updated_hash) {
+    std::string key = access_key_.get_raw_key();
+    std::string auths_json = pre::json::to_json(auths).dump();
+
+    if(updated_hash || vault_info.revision.empty())
+      vault_info.revision = sha1::to_string(sha1::hash(auths_json));
+
+    std::string aad_json = pre::json::to_json(vault_info).dump();
+
+    encrypted_buffer_ = detail::encrypt(key, auths_json, aad_json);
+  }
+
+  vault_info_t vault::get_info(bool verify_sig) {
+    std::string aad;
+
+    if(verify_sig) {
+      auto decr_result = detail::decrypt(access_key_.get_raw_key(), encrypted_buffer_);
+      aad = decr_result.authenticated_data;
+    }
+    else {
+      aad = detail::get_aad_part(encrypted_buffer_);
+    }
+
+    return pre::json::from_json<vault_info_t>(aad);
+  }
+
+  bool vault::update() {
+    bool was_updated = false; 
+    auto decr_result = detail::decrypt(access_key_.get_raw_key(), encrypted_buffer_);
+
+    auths_t auths = pre::json::from_json<auths_t>(decr_result.plain_text);
+    vault_info_t vault_info;
+    auth_t ppk_entry;
+
+    // get the first (*sic*) ppk vault entry
+    auto vault_ppk_enty = std::find_if(auths.begin(), auths.end(), 
+        [](auth_t a){ return a.type == tipi::endpoint_t::TIPI_VAULT_PPK; }); 
+
+    // OH NO! we don't have a public/private key set
+    if(vault_ppk_enty == auths.end()) {   
+      ppk_entry = generate_ecies_keypair_auth();
+      auths.push_back(ppk_entry);   
+      was_updated = true;
+    }
+
+    if(decr_result.is_legacy_vault == true) {      
+      vault_info.public_key = ppk_entry.user; // TIPI_VAULT_PPK."user" is the public key...
+      vault_info.revision = "";
+      was_updated = true;
+    }
+
+    if(was_updated) {
+      save_vault_internal(auths, vault_info);
+    }
+
+    return was_updated;
+  }
+
+  auth_patch_t vault::create_patch(const auth_t& auth) {
+    vault_info_t info = get_info();
+    
+    std::string patch_cleartext_json = pre::json::to_json(auth).dump();
+    std::string patch_hash = sha1::to_string(sha1::hash(info.revision + patch_cleartext_json));
+    std::cout << patch_cleartext_json << std::endl;
+    std::cout << patch_hash << std::endl;
+    std::cout << info.public_key << std::endl;
+    std::cout << info.revision << std::endl;
+
+    auth_patch_t patch{};
+    patch.parent_hash = info.revision;
+    patch.patch_hash = patch_hash;
+    patch.encrypted_patch = detail::encrypt_ecies_message(patch_cleartext_json, info.public_key);
+
+    std::cout << patch.encrypted_patch << std::endl;
+
+    return patch;
+  }
+
+  deserialized_vault_data_t vault::decrypt_and_deserialize_vault() const {
+    auto decr_result = detail::decrypt(access_key_.get_raw_key(), encrypted_buffer_);
+
+    deserialized_vault_data_t result;
+    result.auths = pre::json::from_json<auths_t>(decr_result.plain_text);
+    result.vault_info =  pre::json::from_json<vault_info_t>(decr_result.authenticated_data);
+
+    return result;
   }
 
   std::string vault::get_encrypted_buffer() const { return encrypted_buffer_; }
